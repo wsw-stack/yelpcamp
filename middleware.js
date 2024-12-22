d9333c2e2266f80e848cc051b237ed4ee66ecb66
wsw-stack <1770115879@qq.com>
2024-12-22 10:39:02 -0800

    fix authentication

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 const isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
+        // store the url they are requesting
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must log in to create a new campground!')
         return res.redirect('/login')
     }
--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,14 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        console.log(registeredUser)
-        req.flash('success', 'Welcome to Yelp Camp!')
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if(err) {
+                return next(err)
+            } else {
+                req.flash('success', 'Welcome to Yelp Camp!')
+                res.redirect('/campgrounds')
+            }
+        })
     } catch(e) {
         req.flash('error', e.message)
         res.redirect('/register')
@@ -28,7 +33,9 @@ router.get('/login', (req, res) => {
 // when authentication fails, print a flash message and redirect to the login page
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), async (req, res) => {
     req.flash('success', 'Welcome back!')
-    res.redirect('/campgrounds')
+    const redirectUrl =  req.session.returnTo || '/campgrounds'
+    delete req.session.returnTo
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req, res) => {
